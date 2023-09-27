49724264e7c164c4836ae25bfd5132d7911ace34
Raiden <leonardop_santos@hotmail.com>
2023-09-27 16:28:18 -0300

    Clientes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ClienteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+//Routes Clientes
+Route::middleware('api')->prefix('clientes')->group(function () {
+    Route::get('/', [ClienteController::class, 'index']);
+    Route::post('/', [ClienteController::class, 'store']);
+    Route::get('/{cliente}', [ClienteController::class, 'show']);
+    Route::put('/{cliente}', [ClienteController::class, 'update']);
+    Route::delete('/{cliente}', [ClienteController::class, 'destroy']);
 });
--- tests/Feature/ClienteTest.php
+++ tests/Feature/ClienteTest.php
@@ -2,19 +2,82 @@
 
 namespace Tests\Feature;
 
+use Database\Factories\ClienteFactory;
+use App\Models\Cliente;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ClienteTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase, WithFaker;
 
-        $response->assertStatus(200);
+    /**Listar todos os clientes
+     * @return void
+     */
+
+    public function testListarTodosClientes()
+    {
+        //Criar 5 clientes
+        //Salvar Temporario
+        Cliente::factory()->count(5)->create();
+
+        // usar metodo GET para verificar o retorno
+        $response = $this->getJson('/api/clientes');
+
+        //Testar ou verificar saida
+        $response->assertStatus(200)
+            ->assertJsonCount(5, 'data')
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => ['id', 'created_at', 'updated_at']
+                ]
+            ]);
+    }
+
+         /**
+     * Criar um Cliente
+     */
+    public function testCriarClienteSucesso(){
+
+        //Criar o cliente
+        $cliente = ClienteFactory::new()->make();
+
+        $data = $cliente->getAttributes();
+      
+
+        // Fazer uma requisição POST
+        $response = $this->postJson('/api/clientes', $data);
+
+
+        // Verifique se teve um retorno 201 - Criado com Sucesso
+        // e se a estrutura do JSON Corresponde
+        $response->assertStatus(201)
+            ->assertJsonStructure(['id', 'nomeCliente','idade','sexo','rgIE','cpfCNPJ','email','telefone', 'created_at', 'updated_at']);
+    }
+
+        /**
+     * Teste de criação com falhas
+     *
+     * @return void
+     */
+    public function testCriacaoClienteFalha()
+    {
+        $data = [
+            "nomeCliente" => '',
+            "idade" => '',
+            "sexo" => '',
+            "rgIE" => '',
+            "cpfCNPJ" => '',
+            "email" => '',
+            "telefone" => ''
+        ];
+         // Fazer uma requisição POST
+        $response = $this->postJson('/api/clientes', $data);
+
+        // Verifique se teve um retorno 422 - Falha no salvamento
+        // e se a estrutura do JSON Corresponde
+        $response->assertStatus(422)
+            ->assertJsonValidationErrors(['nomeCliente','idade','sexo','rgIE','cpfCNPJ','email','telefone']);
     }
 }
